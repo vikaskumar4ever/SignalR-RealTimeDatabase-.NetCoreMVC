96f10528f6ed247fb55a48bfb9c689f19a14187e
Vikas Kumar <vikas.kumar2@celebaltech.com>
2022-02-17 11:18:15 +0530

    first commit

---
+++ CoreSignalRApplication/Startup.cs
@@ -0,0 +1,65 @@
+using CoreSignalRApplication.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSignalRApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSignalR();
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Products}/{action=Index}/{id?}");
+
+
+                endpoints.MapHub<SignalrServer>("/SignalrServer");
+            });
+        }
+    }
+}
---
+++ CoreSignalRApplication/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿
+$(() => {
+
+    LoadProdData();
+
+    var connection = new signalR.HubConnectionBuilder().withUrl("/signalrServer").build();
+    connection.start();
+    connection.on("LoadProducts", function () {
+        LoadProdData();
+    })
+
+    function LoadProdData() {
+        debugger;
+        var tr = '';
+        $.ajax({
+            url: '/Products/GetProducts',
+            method: 'GET',
+            success: (result) => {
+                $.each(result, (k, v) => {
+                    tr += `<tr>
+                        <td>${v.ProdName}</td>
+                        <td>${v.Category}</td>
+                        <td>${v.UnitPrice}</td>
+                        <td>${v.StockQty}</td>
+                        <td>
+                        <a href='../Products/Edit?id=${v.ProdId}'>Update</a>
+                        <a href='../Products/Details?id=${v.ProdId}'>View</a>
+                        <a href='../Products/Delete?id=${v.ProdId}'>Delete</a>
+                        </td>
+                    </tr>`
+                })
+                $("#tableBody").html(tr);
+            },
+            error: (error) => {
+                console.log(error)
+            }
+        });
+    }
+
+})
